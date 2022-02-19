1a6f0086b5e7382f08713b354c98f141c8d2be00
Vedad Karalić <61968590+Vedoxx@users.noreply.github.com>
2022-02-19 22:00:56 +0100

    Update taskSeven.java

--- taskSeven.java
+++ taskSeven.java
@@ -2,56 +2,6 @@ import java.util.Scanner;
 
 
 public class taskSeven {
-    static void arrayInputOutput(int sizeOne, int[] array, int[] arraysecond, int sizeSecond) {
-        Scanner object=new Scanner(System.in);
-        int counter=0;
-        System.out.println("Enter integer elements of your first array:");
-        for(int i=0;i<array.length;i++)
-        {
-            array[i]=object.nextInt();
-        }
-        System.out.println("Enter integer elements of your second array:");
-        for(int i=0;i<arraysecond.length;i++)
-        {
-            arraysecond[i]=object.nextInt();
-        }
-
-            int j=0;
-        if(array.length==arraysecond.length)
-            for(int i=0;i<array.length;i++)
-            {
-                System.out.print(array[i]+arraysecond[j]+",");
-                j++;
-            }
-
-        if(array.length>arraysecond.length) {;
-            int k=0;
-            for(int i=0;i<arraysecond.length;i++)
-            {
-                System.out.print(array[i]+arraysecond[k]+",");
-                k++;
-
-            }
-            for(int i=arraysecond.length;i<array.length;i++)
-            {
-                System.out.println(array[i]);
-            }
-        }
-        if(array.length<arraysecond.length)
-        {
-            int k=0;
-            for(int i=0;i<array.length;i++)
-            {
-                System.out.println(array[i]+arraysecond[k]+",");
-                k++;
-            }
-            for(int i=array.length;i<arraysecond.length;i++)
-            {
-                System.out.println(arraysecond[i]);
-            }
-        }
-    }
-
     public static void main(String[] args) {
         Scanner object = new Scanner(System.in);
         System.out.print("Enter the size of your first integer array:");
@@ -60,6 +10,49 @@ public class taskSeven {
         int sizeOfSecondArray = object.nextInt();
         int[] simpleFirstArray = new int[sizeOfFirstArray];
         int[] simpleSecondArray = new int[sizeOfSecondArray];
-        arrayInputOutput(sizeOfFirstArray, simpleFirstArray, simpleSecondArray, sizeOfSecondArray);
+        arrayInput( simpleFirstArray, simpleSecondArray);
+        arrayAdd(simpleFirstArray,simpleSecondArray);
+    }
+    static void arrayInput( int[] array, int[] arraysecond) {
+        Scanner object = new Scanner(System.in);
+        int counter = 0;
+        System.out.println("Enter integer elements of your first array:");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = object.nextInt();
+        }
+        System.out.println("Enter integer elements of your second array:");
+        for (int i = 0; i < arraysecond.length; i++) {
+            arraysecond[i] = object.nextInt();
+        }
+    }
+    static void arrayOutput(int []array,int[] arraySecond)
+    {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + arraySecond[i] + ",");
+        }
+    }
+    static void arrayAddAtEnd(int []array,int[]arraySecond)
+    {
+     for(int i=0;i<arraySecond.length;i++)
+     {
+         System.out.print(array[i]+arraySecond[i]+",");
+     }
+        for(int i=arraySecond.length;i<array.length;i++)
+        {
+            System.out.println(array[i]);
+        }
+    }
+   static void arrayAdd(int[] array, int[] arraysecond) {
+
+        if(array.length==arraysecond.length) {
+           arrayOutput(array,arraysecond);
+        }
+        if(array.length>arraysecond.length) {
+          arrayAddAtEnd(array,arraysecond);
+        }
+        if(array.length<arraysecond.length)
+        {
+            arrayAddAtEnd(arraysecond,array);
+        }
     }
 }
