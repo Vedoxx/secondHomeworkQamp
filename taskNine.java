e567a2f01522487cbc974dcad9c829a45bb44683
Vedad Karalić <61968590+Vedoxx@users.noreply.github.com>
2022-02-20 10:54:47 +0100

    Update taskNine.java

--- taskNine.java
+++ taskNine.java
@@ -2,7 +2,19 @@
 import java.util.Scanner;
 
 public class taskNine {
-    static void arrayInputOutput(int sizeOne, int[] array, int[] arraysecond, int sizeSecond) {
+    public static void main(String[] args) {
+        Scanner object = new Scanner(System.in);
+        System.out.print("Enter the size of your first integer array:");
+        int sizeOfFirstArray = object.nextInt();
+        System.out.print("Enter the size of your second integer array:");
+        int sizeOfSecondArray = object.nextInt();
+        int[] simpleFirstArray = new int[sizeOfFirstArray];
+        int[] simpleSecondArray = new int[sizeOfSecondArray];
+        arrayInput(simpleFirstArray, simpleSecondArray);
+        int result=sum(simpleFirstArray,simpleSecondArray);
+        System.out.println("Total:"+result);
+    }
+    static void arrayInput(int[] array, int[] arraysecond) {
         Scanner object = new Scanner(System.in);
         int counter = 0;
         System.out.println("Enter integer elements of your first array:");
@@ -13,26 +25,18 @@ public class taskNine {
         for (int i = 0; i < arraysecond.length; i++) {
             arraysecond[i] = object.nextInt();
         }
-        int i=0;
+    }
+    static int sum(int[]array,int[]arraysecond)
+    {
         int sum=0;
         for(int j=0;j<arraysecond.length;j++)
         {
-            for(i=0;i<array.length;i++) {
+            for(int i=0;i<array.length;i++) {
 
                 sum = sum + array[i] + arraysecond[j];
             }
-
         }
-        System.out.println("Total:"+sum);
-    }
-    public static void main(String[] args) {
-        Scanner object = new Scanner(System.in);
-        System.out.print("Enter the size of your first integer array:");
-        int sizeOfFirstArray = object.nextInt();
-        System.out.print("Enter the size of your second integer array:");
-        int sizeOfSecondArray = object.nextInt();
-        int[] simpleFirstArray = new int[sizeOfFirstArray];
-        int[] simpleSecondArray = new int[sizeOfSecondArray];
-        arrayInputOutput(sizeOfFirstArray, simpleFirstArray, simpleSecondArray, sizeOfSecondArray);
+        return sum;
     }
 }
+
