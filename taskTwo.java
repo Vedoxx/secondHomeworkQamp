77117481a397d511e10b758a6dbd0313210546ed
Vedad Karalić <61968590+Vedoxx@users.noreply.github.com>
2022-02-22 17:34:35 +0100

    Update taskTwo.java

--- taskTwo.java
+++ taskTwo.java
@@ -4,27 +4,38 @@ import java.util.Scanner;
 public class taskTwo {
   public  static void main(String[] args){
         Scanner object=new Scanner(System.in);
-       char answer='Y';
+        char answer='Y';
         String[] stringArray= new String[]{"First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth", "Eleventh", "Twelfth", "Thirteenth", "Fourteenth", "Fifteenth"};
         System.out.println();
         do {
             System.out.print("Enter the size of your integer array (MAX 15):");
             int sizeOfArray = object.nextInt();
-            if ((sizeOfArray > 15) || (sizeOfArray < 1)) {
+            if(ArrayCheck(sizeOfArray)){
                 System.out.println("ERROR");
             }
-            int[] simpleArray = new int[sizeOfArray];
-            for (int i = 0; i < sizeOfArray; i++) {
-                simpleArray[i] = object.nextInt();
+            else {
+                int[] simpleArray = new int[sizeOfArray];
+                ArrayInput(simpleArray);
+                ArrayOutput(simpleArray, stringArray);
+                System.out.println("Would you like to type again (Yes-y)(No-y)");
+                System.out.print("Answer:");
+                answer = object.next().charAt(0);
             }
-            int j = 0;
-            for (int i = 0; i < sizeOfArray; i++) {
-                System.out.println(stringArray[j] + " in array:" + simpleArray[i]);
-                j++;
-            }
-            System.out.println("Would you like to type again (Yes-y)(No-y)");
-            System.out.print("Answer:");
-            answer = object.next().charAt(0);
         }while(answer=='Y'||(answer=='y'));
+   }
+   static boolean ArrayCheck(int size){
+       return (size > 15) || (size < 1);
+   }
+   static void ArrayInput(int[]simpleArray){
+        Scanner object=new Scanner(System.in);
+        for (int i = 0; i < simpleArray.length; i++) {
+            simpleArray[i] = object.nextInt();
+        }
+    }
+    static void ArrayOutput(int[] array,String[] arraysecond)
+    {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(arraysecond[i] + " in array:" + array[i]);
+        }
     }
 }
