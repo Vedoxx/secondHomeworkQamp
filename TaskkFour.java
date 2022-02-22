351ef80546432478eb035600b49f0362c44be2a0
Vedad Karalić <61968590+Vedoxx@users.noreply.github.com>
2022-02-22 17:46:31 +0100

    Update TaskkFour.java

--- TaskkFour.java
+++ TaskkFour.java
@@ -2,7 +2,20 @@ import java.util.Scanner;
 
 
 public class TaskkFour {
-    static boolean IswordPalindrom(String word)
+    public static void main(String[] args) {
+        Scanner object = new Scanner(System.in);
+
+        System.out.print("Enter the word:");
+        String wordPalindrom = object.nextLine();
+        String lowerWord=wordPalindrom.toLowerCase();
+        boolean result = IsWordPalindrom(lowerWord);
+        if (result) {
+            System.out.println("Word is a palindrom");
+        } else {
+            System.out.println("Word is not a palindrom");
+        }
+    }
+    static boolean IsWordPalindrom(String word)
     {
     int startString=0;
     int endString=word.length()-1;
@@ -17,18 +30,5 @@ public class TaskkFour {
         endString--;
     }
         return true;
-    }
-    public static void main(String[] args) {
-        Scanner object = new Scanner(System.in);
-
-            System.out.print("Enter the word:");
-            String wordPalindrom = object.nextLine();
-            String lowerWord=wordPalindrom.toLowerCase();
-            boolean result = IswordPalindrom(lowerWord);
-            if (result) {
-                System.out.println("Word is a palindrom");
-            } else {
-                System.out.println("Word is not a palindrom");
-            }
     }
 }
