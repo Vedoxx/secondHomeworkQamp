180e66cbdec9e394560987f9d9d4d5ec2ca2a6ba
Vedad Karalić <61968590+Vedoxx@users.noreply.github.com>
2022-02-22 18:29:17 +0100

    Update BonusBonusaTaskTen.java

--- BonusBonusaTaskTen.java
+++ BonusBonusaTaskTen.java
@@ -6,42 +6,37 @@ public class BonusBonusaTaskTen {
         Scanner object = new Scanner(System.in);
         char answer = 'Y';
         do {
-            System.out.print("Enter lower number in range:");
-            int lowerRange = object.nextInt();
-            System.out.print("Enter larger number in range:");
-            int upperRange = object.nextInt();
-            int[] simpleArray = new int[upperRange];
-            for(int i=lowerRange;i<=upperRange;i++){
-                if((i%3==0)&&(i%5==0))
-                {
-                    System.out.println("Fizzbuzz");
-                }
-                else if(i%3==0)
-                {
-                    System.out.println("Fizz");
-                }
-                else if(i%5==0)
-                {
-                    System.out.println("Buzz");
-                }
-                else {
-                    System.out.println(i);
-                }
-            }
-            System.out.println();
-            System.out.println();
-            System.out.println("Numbers not listed:");
-            for(int i=lowerRange;i<=upperRange;i++)
-            {
-                if((i%3==0)||(i%5==0))
-                {
-                    System.out.println(i);
-                }
-
-            }
+            fizzBuzz();
             System.out.println("Would you like to type again (Yes-y)(No-y)");
             System.out.print("Answer:");
             answer = object.next().charAt(0);
         } while (answer == 'Y' || (answer == 'y'));
+    }
+    static void fizzBuzz()
+    {
+        Scanner object = new Scanner(System.in);
+        System.out.print("Enter lower number in range:");
+        int lowerRange = object.nextInt();
+        System.out.print("Enter larger number in range:");
+        int upperRange = object.nextInt();
+        int[] simpleArray = new int[upperRange];
+        for(int i=lowerRange;i<=upperRange;i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("Fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+        System.out.println();
+        System.out.println("Numbers not listed:");
+        for(int i=lowerRange;i<=upperRange;i++) {
+            if((i%3==0)||(i%5==0)) {
+                System.out.println(i);
+            }
+        }
     }
 }
