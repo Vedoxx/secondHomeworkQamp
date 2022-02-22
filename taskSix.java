3cf8271e873e00d111a090c330de664fd57b7e10
Vedad Karalić <61968590+Vedoxx@users.noreply.github.com>
2022-02-22 18:21:23 +0100

    Update taskSix.java

--- taskSix.java
+++ taskSix.java
@@ -3,9 +3,20 @@ import java.util.Scanner;
 
 
 public class taskSix {
-    static void arrayInputOutput(int sizeOne,int []array,int []arraysecond,int sizeSecond){
+    public static void main(String[] args) {
         Scanner object=new Scanner(System.in);
-        int counter=0;
+        System.out.print("Enter the size of your first integer array:");
+        int sizeOfFirstArray = object.nextInt();
+        int[] simpleFirstArray = new int[sizeOfFirstArray];
+        System.out.print("Enter the size of your second integer array:");
+        int sizeOfSecondArray = object.nextInt();
+        int[] simpleSecondArray = new int[sizeOfSecondArray];
+        arrayInput(simpleFirstArray,simpleSecondArray);
+        ArrayOutput(simpleFirstArray,simpleSecondArray);
+        newArray(simpleFirstArray,simpleSecondArray);
+    }
+    static void arrayInput(int[]array,int[]arraysecond){
+        Scanner object=new Scanner(System.in);
         System.out.println("Enter integer elements of your first array:");
         for(int i=0;i<array.length;i++)
         {
@@ -16,32 +27,35 @@ public class taskSix {
         {
             arraysecond[i]=object.nextInt();
         }
-        int[] result=new int[sizeOne+sizeSecond];
+    }
+    static void ArrayOutput(int[] array,int[]arraysecond)
+    {
+        System.out.println();
         System.out.println("First array:");
-        for(int i=0;i<sizeOne;i++)
-        {
-            System.out.print("["+array[i]+"]");
+        for (int k : array) {
+            System.out.print("[" + k + "]");
         }
         System.out.println();
         System.out.println("Second array:");
-        for(int i=0;i<sizeSecond;i++)
-        {
-            System.out.print("["+arraysecond[i]+"]");
+        for (int j : arraysecond) {
+            System.out.print("[" + j + "]");
+        }
+    }
+    static void newArray(int []array,int []arraysecond){
+        int[] result=new int[array.length+arraysecond.length];
+        System.out.println("First array:");
+        for (int j : array) {
+            System.out.print("[" + j + "]");
         }
         System.out.println();
-        System.arraycopy(array, 0, result, 0, sizeOne);
-        System.arraycopy(arraysecond, 0, result, sizeOne, sizeSecond);
+        System.out.println("Second array:");
+        for (int j : arraysecond) {
+            System.out.print("[" + j + "]");
+        }
+        System.out.println();
+        System.arraycopy(array, 0, result, 0, array.length);
+        System.arraycopy(arraysecond, 0, result, array.length, arraysecond.length);
         System.out.println("Combined array:"+Arrays.toString(result));
     }
-    public static void main(String[] args) {
-        Scanner object=new Scanner(System.in);
-        System.out.print("Enter the size of your first integer array:");
-        int sizeOfFirstArray = object.nextInt();
-        System.out.print("Enter the size of your second integer array:");
-        int sizeOfSecondArray = object.nextInt();
-
-        int[] simpleFirstArray = new int[sizeOfFirstArray];
-        int[] simpleSecondArray = new int[sizeOfSecondArray];
-        arrayInputOutput(sizeOfFirstArray,simpleFirstArray,simpleSecondArray,sizeOfSecondArray);
-    }
 }
+
